7568b83b1408dde51524c329fc940c0f8816dccb
JD Tadlock <jt7903@gmail.com>
2023-06-29 13:11:44 -0400

    routes covered

--- server.js
+++ server.js
@@ -5,29 +5,48 @@ const app = express();
 
 console.log(__dirname);
 
+const people = [
+  {
+    id: 1,
+    name: 'JD',
+    age: 43
+  },
+  {
+    id: 2,
+    name: 'Bob',
+    age: 99
+  }
+];
+
+
 // Will send the index.html file as response
 app.get('/', (clientRequestObject, serverResponseObject) => {
   serverResponseObject.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Hello from the about route
-app.get('/about', (clientRequestObject, serverResponseObject) => {
-  serverResponseObject.sendFile(path.join(__dirname, 'style.css'));
+app.get('/api/people', (clientRequestObject, serverResponseObject) => {
+
+  if (!clientRequestObject.query.id) return serverResponseObject.send(people);
+
+  const userObj = people.find(personObj => personObj.id == clientRequestObject.query.id);
+
+  serverResponseObject.send(userObj ? userObj : {
+    message: 'User not found',
+    error: 404
+  });
 });
 
-app.get('/about/test/something/another', (clientRequestObject, serverResponseObject) => {
-  serverResponseObject.send('crazy route');
+app.get('/api/person/:id', (clientRequestObject, serverResponseObject) => {
+  const userObj = people.find(personObj => personObj.id == clientRequestObject.params.id);
+
+  serverResponseObject.send(userObj ? userObj : {
+    message: 'User not found',
+    error: 404
+  });
 });
 
-app.get('/api/data', (clientRequestObject, serverResponseObject) => {
-  serverResponseObject.send({
-    message: 'Wow, we made an API',
-    date: new Date()
-  })
-});
-
-app.get('*', (clientRequestObject, serverResponseObject) => {
-  serverResponseObject.sendFile(path.join(__dirname, 'notfound.html'));
+app.get('/test/:something/val', (clientRequestObject, serverResponseObject) => {
+  serverResponseObject.send('You did it!')
 });
 
 app.listen(3333, () => console.log('Server started on port 3333.'));
