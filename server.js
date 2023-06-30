b57d32c52b2cef5d3a633b72f44db1663d0148f5
JD Tadlock <jt7903@gmail.com>
2023-06-30 15:06:19 -0400

    fixed port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const view_routes = require('./routes/view_routes');
 const api_routes = require('./routes/api_routes');
+const PORT = process.env.PORT || 3333;
 // const path = require('path');
 
 const app = express();
@@ -11,4 +12,4 @@ app.use(express.urlencoded({ extended: false }));
 // Load Routes
 app.use('/', [view_routes, api_routes]);
 
-app.listen(3333, () => console.log('Server started on port 3333.'));
+app.listen(PORT, () => console.log('Server started on port 3333.'));
